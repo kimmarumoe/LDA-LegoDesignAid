0e07183f2cf64fa8318b8f56d2207c650459a85f
kimmarumoe <akfn00321@gmail.com>
2026-01-10 15:23:43 +0900

    refactor: analyze 컴포넌트 폴더 구조 정리

--- frontend/src/components/analyze/guide/BrickGuidePanel.jsx
+++ frontend/src/components/analyze/guide/BrickGuidePanel.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useMemo, useState } from "react";
-import BrickMosaicPreview from "./BrickMosaicPreview.jsx";
+import BrickMosaicPreview from "../../BrickMosaicPreview.jsx";
 
 /*
   BrickGuidePanel
